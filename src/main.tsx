3ed656e81dd72b54aff1a3070faf520e8c08b665
Lodewijck Vogelzang <lodewijck@studiogoes.com>
2025-02-16 12:26:31 +0100

    Configure Prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import { Amplify } from "aws-amplify"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import outputs from "../amplify_outputs.json"
+import App from "./App.tsx"
+import "./index.css"
 
-Amplify.configure(outputs);
+Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
