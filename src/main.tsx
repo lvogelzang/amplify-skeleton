066cbf4c7d49f664c36a65c5314ac5d62b1bca67
Lodewijck Vogelzang <lodewijck@studiogoes.com>
2025-02-16 22:10:17 +0100

    Add theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from "@aws-amplify/ui-react"
+import "@aws-amplify/ui-react/styles.css"
 import { Amplify } from "aws-amplify"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import outputs from "../amplify_outputs.json"
 import App from "./App.tsx"
-import "./index.css"
+import theme from "./theme.ts"
 
 Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 )
